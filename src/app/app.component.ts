b96f9b1cbadebcc43ae902c7be4c516b8719b20b
James Rovel Barbosa <diagjames@gmail.com>
2021-05-18 08:29:19 -0300

    first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; 
+import { Todo } from 'src/models/todo.model';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+
+@Component({ 
+  selector: 'app-root', 
+  templateUrl: './app.component.html', 
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'todo';
+export class AppComponent { 
+  public todos: Todo[] = []; //variavel todo, para tipar usar " : " tipo any (qualquer coisa)
+  public title: String='Minhas Tarefas';
+
+  constructor() { // toda vez que chamar appComponente, chama o todos e o construtor
+    this.todos.push(new Todo(1, 'estudar matematica', false)); //se refere ao escopo da classe, acessa todas variaveis e metodos
+    this.todos.push(new Todo(2, 'lavar roupa', false)); // push adiciona na lista
+    this.todos.push(new Todo(3, 'ir ao mercado', true));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser'; 
 
 import { AppComponent } from './app.component';
 
-@NgModule({
+@NgModule({ 
   declarations: [
     AppComponent
   ],
@@ -11,6 +11,7 @@ import { AppComponent } from './app.component';
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] 
 })
-export class AppModule { }
+export class AppModule { } 
+
